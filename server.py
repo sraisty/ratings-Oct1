2d7fd3442f9679975794710f4812352121bc6130
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-02 16:57:31 -0700

    user login/logout

--- server.py
+++ server.py
@@ -33,9 +33,9 @@ def user_list():
 
 @app.route('/register', methods=["GET"])
 def display_register_form():
-
-
+    """ Displays the user registration form """
     return render_template("registration_form.html")
+
 
 @app.route("/register", methods=['POST'])
 def get_register_info():
@@ -43,12 +43,50 @@ def get_register_info():
     # and then we need to insert them into the Users database
     email = request.form.get("email")
     password = request.form.get("password")
-
+    
+    # first see if the user already exists.  
+    if User.query.filter(User.email==email).first():
+        flash("the email already exists. Please enter another one")
+        return redirect("/register")
+   
+    # if the user doesn't already exist in the database, add the user
     new_user = User(email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
     flash(f"User Added: {email}, {password}")
     return redirect ('/')
+
+@app.route("/login")
+def display_login_form():
+    return render_template('login.html')
+
+@app.route("/do-login")
+def login_user():
+    """ Get user's email and password, make sure that password matches database """
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    user = User.query.filter(User.email==email).first()
+    if user is None:
+        flash("Login failed: invalid user. Please register.")
+        return redirect('/register')
+
+    if user.password==password:
+        # Login succeeded
+        session['user_id'] = user.user_id
+        flash("Login succeeded")
+        return redirect('/')
+
+    # login didn't succeed because of either bad email or bad password
+    flash("Bad password. Please try again.")
+    return redirect('/login')
+
+@app.route("/logout")
+def logout_user():
+    session.pop('user_id', None)
+    flash("You logged out. Bye!")
+    return redirect("/login")
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
